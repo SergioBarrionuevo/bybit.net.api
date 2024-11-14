8100219dd766f5ec1ef14fcfd128029d4a9c0c45
Sergio Barrionuevo <ser.barrionuevo@gmail.com>
2024-11-13 23:14:39 -0600

    Added CancelAllOrdersResponse

--- Src/Common/Models/Trade/Response/OrderResult.cs
+++ Src/Common/Models/Trade/Response/OrderResult.cs
@@ -28,4 +28,40 @@ namespace bybit.net.api.Models.Trade.Response
             return $"Msg [{RetMsg}] Code [{RetCode}] OrderId [{Details?.OrderId}] LinkId [{Details?.OrderLinkId}] Time [{TimeStamp}]";
         }
     }
+
+
+    public class CancelAllOrdersResponse
+    {
+        [JsonProperty("retCode")]
+        public int RetCode { get; set; }
+
+        [JsonProperty("retMsg")]
+        public string RetMsg { get; set; }
+
+        [JsonProperty("result")]
+        public OrderCanceledDetail Result { get; set; }
+
+        [JsonProperty("retExtInfo")]
+        public RetExtInfo RetExtInfo { get; set; }
+
+        public bool IsOk => RetMsg == MessageConstants.Ok;
+
+        [JsonProperty("time")]
+        public long Time { get; set; }
+    }
+
+    public class OrderCanceledDetail
+    {
+        [JsonProperty("list")]
+        public List<OrderDetails> List { get; set; }
+
+        [JsonProperty("success")]
+        public string Success { get; set; }
+    }
+
+    public class RetExtInfo
+    {
+        // If retExtInfo has additional fields, define them here.
+    }
+
 }
